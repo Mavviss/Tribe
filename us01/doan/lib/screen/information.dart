89fbbb192c178cfb303a4f0e105dab31e2bcd06b
nuygahoadong <nuyga28092002@gmail.com>
2022-11-12 20:29:24 +0700

    Giao dien thong tin người chơi

--- us01/doan/lib/screen/information.dart
+++ us01/doan/lib/screen/information.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import 'chonchedo.dart';
+
 class Inforplayer extends StatefulWidget {
   const Inforplayer({super.key});
 
@@ -13,7 +15,54 @@ class _InforplayerState extends State<Inforplayer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/background.jpg"), fit: BoxFit.cover),
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MaterialButton(
+                        onPressed: () {
+                          Navigator.pop(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Picklevel(),
+                            ),
+                          );
+                        },
+                        child: Image.asset(
+                          "images/left-arrow.png",
+                          height: 30,
+                        )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 100),
+                child: Container(
+                      width: MediaQuery.of(context).size.width/1,
+                    height: MediaQuery.of(context).size.height/6,
+                      child: const CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://coinvn.com/wp-content/uploads/2021/05/717_aHR0cHM6Ly9zMy5jb2ludGVsZWdyYXBoLmNvbS91cGxvYWRzLzIwMjEtMDUvZmFmZTZiMjAtZjA1Ny00ODg0LWI1ZTUtOGQ5M2JkNWViZDQ3LmpwZw.jpg"),
+                      ),
+                    ),
+                  
+              ),
+              Row(
+                children: [
+                  Text('Họ và tên'),
+                  Icon(Icons.edit)
+                ],
+              ),
+              TextField(),
+              Text(' Ngày tạo tài khoản: ')
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
