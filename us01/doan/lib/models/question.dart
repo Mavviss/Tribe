d504c033dc9a63d56e3e2492af15d233acc0c5ee
PhoBoKho <0306201431@caothang.edu.vn>
2022-11-12 18:32:11 +0700

    them giao dien con lai

---
+++ us01/doan/lib/models/question.dart
@@ -0,0 +1,32 @@
+class Question
+{
+  final String quest;
+  final List<Answer> answers;
+  bool isLocked;
+  Answer? selectedOption;
+
+  Question({
+    required this.quest,
+    required this.answers,
+    this.isLocked = false,
+    this.selectedOption,
+  });
+}
+class Answer{
+  final String text;
+  final bool isCorrect;
+  const Answer({
+    required this.text,
+    required this.isCorrect,
+  });
+
+}
+final questions = [
+  Question(quest: 'quest', answers: [
+    const Answer(text: 'Câu trả lời 1', isCorrect: true),
+    const Answer(text: 'Câu trả lời 2', isCorrect: true),
+    const Answer(text: 'Câu trả lời 3', isCorrect: true),
+    const Answer(text: 'Câu trả lời 4', isCorrect: true),
+  ]
+  )
+];
---
+++ us01/doan/lib/screen/information.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Inforplayer extends StatefulWidget {
+  const Inforplayer({super.key});
+
+  @override
+  State<Inforplayer> createState() => _InforplayerState();
+}
+
+class _InforplayerState extends State<Inforplayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
