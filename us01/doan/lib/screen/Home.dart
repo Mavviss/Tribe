b87da2819b4f8e05f1d6932ab926bb72f00b0d2d
Mavviss <0306201426@caothang.edu.vn>
2022-11-12 16:08:38 +0700

    Them push mang hinh home

--- us01/doan/lib/screen/Home.dart
+++ us01/doan/lib/screen/Home.dart
@@ -4,7 +4,8 @@ import 'package:flutter/src/widgets/framework.dart';
 //import 'package:tribe_duan/layout/rank.dart';
 //import 'package:tribe_duan/layout/shop.dart';
 //import 'chonchedo.dart';
-
+import '../screen/forgot_password.dart';
+import '../screen/login.dart';
 class Start extends StatefulWidget {
   const Start({super.key});
   @override
@@ -27,44 +28,44 @@ class _StartState extends State<Start> {
                 Text("Chào mừng bạn đến với bộ tộc TriBe"),
                 
                 SizedBox(height: size.height/20,),
-                // MaterialButton(
-                //   onPressed: (() {
-                //     Navigator.push(
-                //       context,
-                //       MaterialPageRoute(
-                //         builder: (context) => const Picklevel(),
-                //       ),
-                //     );
-                //   }),
-                //   height: size.height / 20,
-                //   minWidth: size.height / 5,
-                //   color: const Color.fromRGBO(240, 240, 240, 1),
-                //   child: const Icon(Icons.play_circle_outline),
-                // ),
+                MaterialButton(
+                  onPressed: (() {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const forgotPasswordPage(),
+                      ),
+                    );
+                  }),
+                  height: size.height / 20,
+                  minWidth: size.height / 5,
+                  color: const Color.fromRGBO(240, 240, 240, 1),
+                  child: const Icon(Icons.play_circle_outline),
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    // GestureDetector(
-                    //   onTap: () {
-                    //     Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    //         builder: (BuildContext context) => const Shop()));
-                    //   },
-                    //   child: Image.asset(
-                    //     "images/Shop.png",
-                    //     width: size.width / 8,
-                    //   ),
-                    // ),
-                    // GestureDetector(
-                    //   onTap: () {
-                    //     Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    //         builder: (BuildContext context) => const Rank()));
-                    //   },
-                    //   child: Image.asset(
-                    //     "images/Champion.png",
-                    //     height: size.height / 2.2,
-                    //     width: size.width / 8,
-                    //   ),
-                    // ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (BuildContext context) => const LoginPage()));
+                      },
+                      child: Image.asset(
+                        "images/Shop.png",
+                        width: size.width / 8,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (BuildContext context) => const LoginPage()));
+                      },
+                      child: Image.asset(
+                        "images/Champion.png",
+                        height: size.height / 2.2,
+                        width: size.width / 8,
+                      ),
+                    ),
                   ],
                 ),
               ]),
