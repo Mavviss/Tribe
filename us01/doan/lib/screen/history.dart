8a62fbd983607527865b2f0d88e5a8e986cb930f
PhoBoKho <0306201431@caothang.edu.vn>
2022-11-27 14:54:00 +0700

    them header cho shop

--- us01/doan/lib/screen/history.dart
+++ us01/doan/lib/screen/history.dart
@@ -1,5 +1,7 @@
 import 'package:doan/screen/Rank.dart';
 import 'package:flutter/material.dart';
+import 'package:gradient_borders/box_borders/gradient_box_border.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class History extends StatefulWidget {
   const History({
@@ -22,14 +24,211 @@ class _HistoryState extends State<History> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Lịch sử đấu',
-          style: TextStyle(color: Colors.red),
-          textAlign: TextAlign.center,
-        ),
-        iconTheme: IconThemeData(color: Colors.black),
-        backgroundColor: Colors.white,
-        leading: Icon(Icons.keyboard_return_outlined),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+            child: Row(
+              children: [
+                Container(
+                  child: Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        //container chứa ảnh
+                        child: Container(
+                            width: 100,
+                            height: 24,
+                            decoration: BoxDecoration(
+                                //color: Colors.blueGrey,
+                                gradient: LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                      Color(0xffCDC1C5),
+                                      Color(0xffCDC1C5)
+                                    ]),
+                                border: Border.all(
+                                  width: 1.5,
+                                ),
+                                borderRadius: BorderRadius.circular(5)),
+                            child: Center(
+                                child: Text(
+                              'Mavis',
+                              style: TextStyle(color: Colors.black),
+                            ))),
+                      ),
+                      Positioned(
+                        top: 0,
+                        child: GestureDetector(
+                          onTap: () {
+                            // ontap hình avatar
+                          },
+
+                          //chỗ bắt đầu
+                          child: Container(
+                            decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                      Color(0xffb92b27),
+                                      Color(0xff1565c0)
+                                    ]),
+                                border: Border.all(
+                                  width: 1,
+                                ),
+                                borderRadius: BorderRadius.circular(7)),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  border: GradientBoxBorder(
+                                    gradient: LinearGradient(colors: [
+                                      Color(0xffee9ca7),
+                                      Color(0xffffdde1)
+                                    ]),
+                                    width: 2,
+                                  ),
+                                  borderRadius: BorderRadius.circular(7)),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    border: Border.all(width: 1),
+                                    borderRadius: BorderRadius.circular(7)),
+                                child: Image.network(
+                                  ("https://scontent.fsgn2-5.fna.fbcdn.net/v/t39.30808-1/278377002_3147115085544105_8279216043983986656_n.jpg?stp=dst-jpg_s200x200&_nc_cat=106&ccb=1-7&_nc_sid=7206a8&_nc_ohc=K4q5CEP3Je8AX-knRaH&_nc_ht=scontent.fsgn2-5.fna&oh=00_AfCCCoDM_3SvKyYSK5zptfJ7i9sNmBl59BMFBXvE-6m78g&oe=6384D8E6"),
+                                  width: 30,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                //contaier chứa tim
+                Container(
+                  child: Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                            width: 100,
+                            height: 24,
+                            decoration: BoxDecoration(
+                                //  color: Colors.blueGrey,
+                                gradient: LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                      Color(0xffCDC1C5),
+                                      Color(0xffCDC1C5)
+                                    ]),
+                                border: Border.all(
+                                  width: 1.5,
+                                ),
+                                borderRadius: BorderRadius.circular(5)),
+                            child: Center(
+                                child: Text(
+                              '10',
+                              style: TextStyle(color: Colors.black),
+                            ))),
+                      ),
+                      Positioned(
+                        top: 2,
+                        bottom: 2,
+                        left: 0,
+                        child: SizedBox(
+                            width: 27,
+                            height: 10,
+                            child: SvgPicture.asset(
+                              "images/favorite.svg",
+                            )),
+                      ),
+                      Positioned(
+                        top: -4,
+                        right: -5,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: SizedBox(
+                            height: 20,
+                            child: SvgPicture.asset(
+                              "images/plus.svg",
+                            ),
+                          ),
+                          tooltip: "Mua sinh mạng",
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                //container chứa coin
+                Container(
+                  child: Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                            width: 100,
+                            height: 24,
+                            decoration: BoxDecoration(
+                                // color: Colors.blueGrey,
+                                gradient: LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                      Color(0xffCDC1C5),
+                                      Color(0xffCDC1C5)
+                                    ]),
+                                border: Border.all(
+                                  width: 1.5,
+                                ),
+                                borderRadius: BorderRadius.circular(5)),
+                            child: Center(
+                                child: Text('10',
+                                    style: TextStyle(color: Colors.black)))),
+                      ),
+                      Positioned(
+                        top: 0,
+                        bottom: 3,
+                        left: 0,
+                        child: SizedBox(
+                          width: 33,
+                          height: 25,
+                          child: SvgPicture.asset(
+                            "images/leaf.svg",
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: -4,
+                        right: -5,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: SizedBox(
+                              height: 20,
+                              child: SvgPicture.asset(
+                                "images/plus.svg",
+                              )),
+                          tooltip: "Mua ngân lượng",
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.settings,
+                    color: Colors.black,
+                  ),
+                  tooltip: "Cài đặt",
+                )
+              ],
+            ),
+          ),
+        ],
       ),
       body: Container(
         width: double.infinity,
@@ -129,7 +328,9 @@ class _itemlichsuchoidonState extends State<itemlichsuchoidon> {
       child: Container(
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(8)),
+          borderRadius: BorderRadius.all(
+            Radius.circular(8),
+          ),
           border: Border.all(
             color: Colors.black,
             width: 1.0,
